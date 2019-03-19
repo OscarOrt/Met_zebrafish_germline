b1ddc390dc2d5b4a183a1c2d4415d851cef5bf3d
OscarO <ojavieror@gmail.com>
2019-03-19 14:52:20 +1300

    Update 19March2019
    Paper resubmitted, code and files updated.

--- Analysis_fem-rDNA_zebrafish.R
+++ Analysis_fem-rDNA_zebrafish.R
@@ -2,36 +2,39 @@
 library(ggbio)
 library(rtracklayer)
 library(biovizBase)
-library(BSgenome.Drerio.UCSC.danRer10)
+library(BSgenome.Drerio.UCSC.GRCz11)
 library(dplyr)
 library(reshape)
 
 # Set directory
 setwd("~/Documents/GitHub/Met_zebrafish_germline/Data")
 
+setwd("~/Documents/GitHub/Met_zebrafish_germline")
+setwd("~/Syncplicity Folders/Met_rDNA_zebrafish_Ortega2018/Revised")
+
 ############################################################
 # Grand plot                                               #
 ############################################################
 
 # Load data and transform variables
-data_Fig4A <- read.csv2("probes_1Mb.txt",sep = "\t",stringsAsFactors = FALSE,dec=".")
-data_Fig4A$norm_21d_egfp <- data_Fig4A$X21d_egfp/(sum(data_Fig4A$X21d_egfp)/nrow(data_Fig4A))
-data_Fig4A$norm_25d_egfp <- data_Fig4A$X25d_egfp/(sum(data_Fig4A$X25d_egfp)/nrow(data_Fig4A))
-data_Fig4A$norm_28d_egfp <- data_Fig4A$X28d_egfp/(sum(data_Fig4A$X28d_egfp)/nrow(data_Fig4A))
+data_1Mb_probes <- read.csv2("Data/Probes_1Mb/Probes_1Mb.txt",sep = "\t",stringsAsFactors = FALSE,dec=".")
+data_1Mb_probes$norm_21d_egfp <- data_1Mb_probes$X21d_egfp/(sum(data_1Mb_probes$X21d_egfp)/nrow(data_1Mb_probes))
+data_1Mb_probes$norm_25d_egfp <- data_1Mb_probes$X25d_egfp/(sum(data_1Mb_probes$X25d_egfp)/nrow(data_1Mb_probes))
+data_1Mb_probes$norm_28d_egfp <- data_1Mb_probes$X28d_egfp/(sum(data_1Mb_probes$X28d_egfp)/nrow(data_1Mb_probes))
 
 # Create genomic ranges table
-gr.table <- transformDfToGr(data_Fig4A, seqnames = "Chromosome", start = "Start",end="End")
+gr.table <- transformDfToGr(data_1Mb_probes, seqnames = "Chromosome", start = "Start",end="End")
 head(gr.table)
 
 # Calculate seq lenghts
 seqlengths(gr.table)
 
-danRer10IdeogramCyto <- getIdeogram("danRer10")
-seqlengths(gr.table) <- as.numeric(seqlengths(danRer10IdeogramCyto)[1:26])
+danRer11IdeogramCyto <- getIdeogram("danRer11")
+seqlengths(gr.table) <- as.numeric(seqlengths(danRer11IdeogramCyto)[1:26])
 
 seqlengths(gr.table)
 
-# Plot GrandLinear GSC 21d
+# Plot GrandLinear germline 21d
 plotGrandLinear(gr.table, geom = "area",aes(y = log10(norm_21d_egfp+1))) + ylab("") + ylim(0,3) + theme_gray()+
   theme(plot.background = element_blank(),
         panel.background = element_rect(fill = "white",
@@ -43,7 +46,7 @@ plotGrandLinear(gr.table, geom = "area",aes(y = log10(norm_21d_egfp+1))) + ylab(
   theme(axis.line.x = element_line(color="black", size = 0.5),
         axis.line.y = element_line(color="black", size = 0.5))
 
-# Plot GrandLinear GSC 25d
+# Plot GrandLinear germline 25d
 plotGrandLinear(gr.table, geom = "area",aes(y = log10(norm_25d_egfp+1))) + ylab("") + ylim(0,3) + theme_gray()+
   theme(plot.background = element_blank(),
         panel.background = element_rect(fill = "white",
@@ -72,9 +75,9 @@ plotGrandLinear(gr.table, geom = "area",aes(y = log10(norm_28d_egfp+1))) + ylab(
 ############################################################
 # Ideogram                                                 #
 ############################################################
-danRer10IdeogramCyto <- getIdeogram("danRer10", cytoband = FALSE)
+danRer11IdeogramCyto <- getIdeogram("danRer11", cytoband = FALSE)
 
-plotIdeogram(danRer10IdeogramCyto,"chr4",xlabel = TRUE,zoom.region = c(76310000, 76335000),
+plotIdeogram(danRer11IdeogramCyto,"chr4",xlabel = TRUE,zoom.region = c(start = 77545000, end = 77570000),
              color = "red",fill = "red",alpha = 0.5,size = 2,aspect.ratio = 1/10)
 
 # 750 x 125
@@ -83,13 +86,23 @@ plotIdeogram(danRer10IdeogramCyto,"chr4",xlabel = TRUE,zoom.region = c(76310000,
 # Coverage plot                                            #
 ############################################################
 
-bg <- BSgenome.Drerio.UCSC.danRer10
-gr <- GRanges(seqnames = "4", ranges = IRanges(start = 76310000, end = 76335000))
+bg <- BSgenome.Drerio.UCSC.GRCz11
+gr <- GRanges(seqnames = "4", ranges = IRanges(start = 77545000, end = 77570000))
 
-CTOT.bam <- "CTOT.bam"
+CTOT.bam <- "Data/Mapping/CTOT.bam"
+
+# Limits
+#gr <- GRanges(seqnames = "4", ranges = IRanges(start = 77549888, end = 77549895))
+#gr <- GRanges(seqnames = "4", ranges = IRanges(start = 77567273, end = 77567279))
+
 autoplot(CTOT.bam, bsgenome = bg,which = gr, geom = "area") + xlim(gr) + ylim(c(0,100))
 
-CTOB.bam <- "CTOB.bam"
+CTOB.bam <- "Data/Mapping/CTOB.bam"
+
+# Limits
+#gr <- GRanges(seqnames = "4", ranges = IRanges(start = 77549894, end = 77549900))
+#gr <- GRanges(seqnames = "4", ranges = IRanges(start = 77567273, end = 77567279))
+
 autoplot(CTOB.bam, bsgenome = bg,which = gr, geom = "area") + xlim(gr) + ylim(c(0,100))
 
 # 700 x 200
@@ -99,13 +112,13 @@ autoplot(CTOB.bam, bsgenome = bg,which = gr, geom = "area") + xlim(gr) + ylim(c(
 ############################################################
 
 # Load data and transform variables
-Wilson_data <- read.csv2("Wilson_data.csv",sep = ",",stringsAsFactors = FALSE,dec=".")
-str(Wilson_data)
+Wilson_data <- read.csv2("Data/Wilson_data.csv",sep = ",",stringsAsFactors = FALSE,dec=".")
 
 p <- ggplot(Wilson_data, aes(Coordinates,P_value))
-p + geom_point(aes(colour = factor(Strain)),size = 3) +
+p + geom_point(aes(colour = factor(Strain),shape = factor(Strain)),size = 3) +
+  scale_shape_manual(values=c(rep(19,2),108,rep(19,3))) +
   geom_hline(yintercept=-log(0.01), linetype="dashed", color = "black") +
-  xlim(73900000, 76625712)
+  xlim(75000000,78090000)
 
 # 700 x 550
 
@@ -114,17 +127,17 @@ p + geom_point(aes(colour = factor(Strain)),size = 3) +
 ############################################################
 
 # Load data and transform variables
-data_fig_5b <- read.csv2("data_fem_rDNA.csv",sep = ",",stringsAsFactors = FALSE,dec=",")
+data_fem_rDNA <- read.csv("Data/data_fem_rDNA.csv",sep = ",",stringsAsFactors = FALSE)
 
-data_cleaned_fig_5b = data_fig_5b[data_fig_5b$CG_calls>10,]
+data_cleaned_fem_rDNA = data_fem_rDNA[data_fem_rDNA$n_calls_fem_rDNA>10,]
 
-data_cleaned_fig_5b$Amplification
-p <- ggplot(data_cleaned_fig_5b, aes(Amplification,CG_met))
+p <- ggplot(data_cleaned_fem_rDNA, aes(per_reads_fem_rDNA,met_rDNA))
 
 p + geom_point(aes(colour = factor(Label)),size = 6) +
-  scale_color_manual(values=c("#808080","#FFB6C1","#add8e6","#FF00FF",
-                              "#ff0000","#0000ff","#00AA00"))+
+  scale_color_manual(values=c("#00AA00","#FF00FF","#808080","#FFB6C1",
+                              "#ff0000","#add8e6","#0000ff"))+
   geom_vline(xintercept = 1, linetype= "dashed", color = "black")+
+  geom_hline(yintercept = 35, linetype= "dashed", color = "black")+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
@@ -132,20 +145,203 @@ p + geom_point(aes(colour = factor(Label)),size = 6) +
         axis.line = element_line(size=0.5)) +
   scale_x_log10()
 
-##########################################################
+data_cleaned_fem_rDNA_germline = data_cleaned_fem_rDNA[data_cleaned_fem_rDNA$type=="egfp",]
+str(data_cleaned_fem_rDNA_germline)
+
+sum(data_cleaned_fem_rDNA_germline$per_reads_fem_rDNA>1)
+
+# 1000 x 800 (937.5 x 748.75) -> 1344.086 x 1073.477
+
+# Group 1
+
+group_1 <- data_cleaned_fem_rDNA[data_cleaned_fem_rDNA$met_rDNA > 35,]
+mean(group_1$met_rDNA)
+sd(group_1$met_rDNA)
+
+# Group 2
+
+group_2 <- data_cleaned_fem_rDNA[(data_cleaned_fem_rDNA$met_rDNA < 35)&
+                                   (data_cleaned_fem_rDNA$per_reads_fem_rDNA < 1),]
+mean(group_2$met_rDNA)
+sd(group_2$met_rDNA)
+min(group_2$per_reads_fem_rDNA)
+max(group_2$per_reads_fem_rDNA)
+
+# Group 3
+
+group_3 <- data_cleaned_fem_rDNA[data_cleaned_fem_rDNA$per_reads_fem_rDNA > 1,]
+mean(group_3$met_rDNA)
+sd(group_3$met_rDNA)
+
+############################################################
 # Summary amplification                                    #
 ############################################################
 
 # Load data and transform variables
-fig_6b <- read.csv2("Data_summary_B.csv",sep = ",",stringsAsFactors = FALSE,dec=".")
-str(fig_6b)
 
-p <- ggplot(fig_6b, aes(Day,Amplification))
-p + geom_point(aes(colour = factor(Sex)),size = 8) +
-  scale_color_manual(values=c("#ff0000","#0000ff","#808080"))+
-  xlim(0,61) + ylim(0,25) +
+data_fem_rDNA <- read.csv("Data/data_fem_rDNA.csv",sep = ",",stringsAsFactors = FALSE)
+
+data_fem_rDNA_germline = data_fem_rDNA[data_fem_rDNA$type == "egfp",]
+
+p <- ggplot(data_fem_rDNA_germline, aes(Day,per_reads_fem_rDNA ))
+
+p + geom_point(aes(colour = factor(sex)),size = 4) +
+  scale_color_manual(values=c("#ff0000","#0000ff","#00AA00"))+
+  xlim(0,65) + ylim(0,25) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         panel.border = element_blank(),
         axis.line = element_line(size=0.5))
+
+# 1200 x 600
+
+############################################################
+# Simulation Low coverage WGBS                             #
+############################################################
+
+# Figure simulation
+
+# Data 1 - Sperm
+data_sperm <- read.csv("Data/Sampling/sperm_simulation.csv",sep = ",", stringsAsFactors = FALSE)
+data_sperm$Total_Cs_CpG = data_sperm$Total_CsM_CpG + data_sperm$Total_CsU_CpG
+data_sperm$CsM_CpG = data_sperm$Total_CsM_CpG * 100 / data_sperm$Total_Cs_CpG
+
+data_sperm_clean = data.frame("ID" = data_sperm$Sample, "Pred_Cs" = data_sperm$Group,
+                              "Total_Cs_CpG" = data_sperm$Total_Cs_CpG,
+                              "CsM_CpG" = data_sperm$CsM_CpG)
+
+data_sperm_summary <- as.data.frame(data_sperm_clean %>% group_by(Pred_Cs) %>%
+                                      summarise(mean = mean(CsM_CpG, na.rm = TRUE),
+                                                quantile_99_95 = quantile(CsM_CpG, c(.9995)),
+                                                quantile_00_05 = quantile(CsM_CpG, c(.0005)),
+                                                dif = quantile_99_95 - quantile_00_05,
+                                                ME = dif / 2
+                                      ))
+
+data_sperm_summary
+
+# Data 2 - Muscle
+data_muscle <- read.csv("Data/Sampling/muscle_simulation.csv",sep = ",", stringsAsFactors = FALSE)
+data_muscle$Total_Cs_CpG = data_muscle$Total_CsM_CpG + data_muscle$Total_CsU_CpG
+data_muscle$CsM_CpG = data_muscle$Total_CsM_CpG * 100 / data_muscle$Total_Cs_CpG
+
+data_muscle_clean = data.frame("ID" = data_muscle$Sample, "Pred_Cs" = data_muscle$Group,
+                               "Total_Cs_CpG" = data_muscle$Total_Cs_CpG,
+                               "CsM_CpG" = data_muscle$CsM_CpG)
+
+data_muscle_summary <- as.data.frame(data_muscle_clean %>% group_by(Pred_Cs) %>%
+                                       summarise(mean = mean(CsM_CpG, na.rm = TRUE),
+                                                 quantile_99_95 = quantile(CsM_CpG, c(.9995)),
+                                                 quantile_00_05 = quantile(CsM_CpG, c(.0005)),
+                                                 dif = quantile_99_95 - quantile_00_05,
+                                                 ME = dif / 2
+                                       ))
+
+data_muscle_summary
+
+
+data_summary <- rbind(data_sperm_summary,data_muscle_summary)
+data_summary["Tissue"] = c(rep("Sperm",12),rep("Muscle",12))
+
+# Analyse data
+data_all = rbind(data_sperm_clean, data_muscle_clean)
+data_all["Tissue"] = c(rep("Sperm",12000),rep("Muscle",12000))
+
+x_1 = c(data_sperm_summary$Pred_Cs, rev(data_sperm_summary$Pred_Cs))
+y_1 = c(data_sperm_summary$quantile_99_95, rev(data_sperm_summary$quantile_00_05))
+
+pol_data_sperm = data.frame("x" = x_1, "y"= y_1, "group" = c(rep("low",12),rep("high",12)))
+
+x_2 = c(data_muscle_summary$Pred_Cs, rev(data_muscle_summary$Pred_Cs))
+y_2 = c(data_muscle_summary$quantile_99_95, rev(data_muscle_summary$quantile_00_05))
+
+pol_data_muscle = data.frame("x" = x_2, "y"= y_2, "group" = c(rep("low",12),rep("high",12)))
+
+pol_all = rbind(pol_data_sperm, pol_data_muscle)
+pol_all["Tissue"] = c(rep("Sperm",24),rep("Muscle",24))
+
+# Plot simulation
+plot_A = ggplot(data_all, aes(x = Total_Cs_CpG, y = CsM_CpG, color = Tissue)) +
+  geom_point(alpha = 0.5) +
+  xlab("Number of Cs in CG context") + ylab("CG methylation (%)") +
+  theme(
+    axis.title = element_text(size = 30),
+    axis.text = element_text(size = 30),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 30),
+    axis.line = element_line(size = 0.5)
+  ) + 
+  scale_color_manual(values=c('#F8766D','#619CFF')) +
+  geom_polygon(data = pol_all, aes(x = x, y = y, fill = Tissue, color = NA),alpha = 0.2) +
+  scale_fill_manual(values=c('#F8766D','#619CFF')) +
+  ylim(50,100) + 
+  geom_hline(yintercept=94.34,linetype="dashed", color="blue") + 
+  geom_hline(yintercept=81.12,linetype="dashed", color="red")
+
+# 1100 x 1000
+plot_A
+
+# Construct model
+x1 = data_sperm_summary$Pred_Cs
+y1 = data_sperm_summary$ME
+xy1 = data.frame("x" = x1, "y" = y1)
+
+mod_new1 <- lm(y1 ~ I(1/sqrt(x1))+0, xy1)
+mod_new1$coefficients/100
+
+x2 = data_muscle_summary$Pred_Cs
+y2 = data_muscle_summary$ME
+xy2 = data.frame("x" = x2, "y" = y2)
+
+mod_new2 <- lm(y2 ~ I(1/sqrt(x2))+0, xy2)
+mod_new2$coefficients/100
+
+options(digits=4)
+
+x_mod_1 <- seq(0,30000,10)
+y_mod_1 <- c()
+for (i in seq(0,30000,10)){
+  y_temp = 1.207/(sqrt(i))*100
+  y_mod_1 = c(y_mod_1,y_temp)
+}
+
+x_mod_2 <- seq(0,30000,10)
+y_mod_2 <- c()
+for (i in seq(0,30000,10)){
+  y_temp = 2.109/(sqrt(i))*100
+  y_mod_2 = c(y_mod_2,y_temp)
+}
+
+mod_1 = data.frame("X_mod"=x_mod_1,"Y_mod"=y_mod_1)
+mod_2 = data.frame("X_mod"=x_mod_2,"Y_mod"=y_mod_2)
+
+mod_all <- rbind(mod_1,mod_2)
+mod_all["Tissue"] = c(rep("Sperm",3001),rep("Muscle",3001))
+
+plot_B = ggplot(data_summary, aes(x = Pred_Cs, y = ME, color = Tissue)) +
+  geom_point(alpha = 0.9, size = 4) +
+  geom_line(data = mod_all, aes(x = X_mod, y = Y_mod, color = Tissue)) + 
+  xlab("Number of Cs in CG context") + ylab("Margin of error (± %) 99% CI") +
+  theme(
+    axis.title = element_text(size = 30),
+    axis.text = element_text(size = 30),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    legend.title = element_blank(),
+    legend.text = element_text(size = 30),
+    axis.line = element_line(size = 0.5)
+  ) +
+  geom_hline(yintercept=1,linetype="dashed", color="blue", size = 0.75) +
+  geom_hline(yintercept=2.5,linetype="dashed", color="green", size = 0.75) +
+  geom_hline(yintercept=5,linetype="dashed", color="red",size = 0.75) +
+  geom_vline(xintercept=10000,linetype="dashed", color="black",size = 0.5) +
+  ylim(0,30) +
+  scale_color_manual(values=c('#F8766D','#619CFF'))
+
+# 1100 x 1000
+plot_B
--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ The code is implemented in R (v3.4.4) and the following libraries:
 - ggbio v1.26.1
 - rtracklayer v1.38.3
 - biovizBase v 1.26.0
-- BSgenome.Drerio.UCSC.danRer10 v1.4.2
+- BSgenome.Drerio.UCSC.danRer11 (Forged using BSgenome 1.4.2)
 - dplyr 0.7.6
 - reshape 0.8.7
 - ggplot2 3.0.0
@@ -20,9 +20,9 @@ Probes of 1 Mb through the zebrafish genome were created in SeqMonk (v1.43.0)to 
 This code plots an ideogram highlighting the peak observed in the previous analysis.
 
 - Coverage plot
-BAM files obtained by mapping reads wit Bowtie2 v(2.3.2) as mention in the manuscript were used for this analysis. This code plots the reads mapped to the genomic region of interest.
+BAM files obtained by mapping reads with Bowtie2 v(2.3.2) as mention in the manuscript were used for this analysis. This code plots the reads mapped to the genomic region of interest.
 
-- Wilson data fem-rDNA
+- Wilson data fem rDNA
 This code plots the location of the amplified region and its relation with the sex linked SNPs obtained in a previous study (Wilson et al, 2014)
 
 - Relation amplification - methylation
@@ -30,3 +30,7 @@ This code plots the relation between rDNA and amplification in the zebrafish ger
 
 - Summary amplification
 This code plots a summary for the results obtained.
+
+- Simulation Low coverage WGBS
+
+This code plots the simulation for sperm and muscle WGBS data. Additionally it estimates the confidence interval for different sample sizes.
